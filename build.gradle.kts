ca83221bda2e893a967885c9c51f50eb44cc3d5d
shalva97 <shalva258@gmail.com>
2023-10-29 20:50:41 +0400

    add realms and reduce noise in console

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    id("io.realm.kotlin") version "1.11.0"
 }
 
 group = "io.github.shalva97"
@@ -13,8 +14,17 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation ("com.github.teamnewpipe:NewPipeExtractor:0.22.7")
+    implementation("com.github.teamnewpipe:NewPipeExtractor:0.22.7")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+    testImplementation("io.realm.kotlin:library-base:1.11.0")
+}
+
+allprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
 }
 
 tasks.test {
